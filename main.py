e3d0b782664f6253cdce82d4744f69653471cc36
J.Malkowski <jak.malkowski@gmail.com>
2022-06-05 19:20:12 +0200

    Patience mechanism

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 
+from cv2 import threshold
 # Load the cascade
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 # eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
@@ -10,21 +11,33 @@ cap = cv2.VideoCapture(0)
 # To use a video file as input 
 # cap = cv2.VideoCapture('filename.mp4')
 
+
+threshold = 0
+    
+
 while True:
     _, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    faces = [f for f in faces if f[2]*f[3] > 100000] # prevent smaller objects:
+    faces = [f for f in faces if f[2]*f[3] > 500] # prevent smaller objects:
+
     if faces == []:
-        print("distracted!!! \a")
-    # for (x, y, w, h) in faces:
-    #     if (w*h > 100000) 
-    #         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        threshold += 1
+    else:
+        threshold = 0
 
-    time.sleep(0.1)
+    if threshold >= 10 :
+        print("DISTRACTED ! ! ! \n")
+        print('\a')
 
-    # cv2.imshow('img', img)
+    for (x, y, w, h) in faces:
+         if (w*h > 100000):
+             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
     if k==27:
         break
+    time.sleep(0.5)
+
 cap.release()
